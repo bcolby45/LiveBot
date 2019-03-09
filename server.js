6c3032b88368e85b6dd2c9bc3dc7be2926e4fcf2
Brandon Colby <>
2019-03-09 14:12:33 -0500

    LiveBot 1.10

--- server.js
+++ server.js
@@ -1,207 +1,136 @@
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+'use strict'
 const express = require('express');
 const app = express().enable('trust proxy');
 const http = require('http');
 const https = require('https');
 const webServer = http.createServer(app);
 const cors = require('cors');
-const webSocketServer = require('ws').Server;
-const WebSocketClient = require('websocket').client;
-const EventSource = require("eventsource");
+const WebSocketServer = require('ws').Server;
+const EventSource = require('eventsource');
 const fbsdk = require('facebook-sdk');
-const tmi = require('tmi.js');
-const ws = new webSocketServer({
-    server: webServer
+const ws = new WebSocketServer({
+  server: webServer,
 });
-
+const appID = ``;
+const appSecret = ``;
+const postId = ``;
+const accessToken = `EAAFhceAP8u0BAID95sV1oYI9XrjM3zDT5s4t9WX6E8O1ZCRXUU2ZBOIcJZAWkL2SrNZBy4SimCsY2f8ZCMerfTNR566eETriH0R67QSJyv5ZCub5wZCTkpG18k6J0FC7ZCXJ5PmYrIfGqUtVo3o4vVQ9e2g2v4h5K0UZD`;
 process.on('uncaughtException', function(err) { // I don't like killing the process when one of my libraries fucks up.
-    console.log("UNCAUGHT EXCEPTION\n" + err);
+  console.log('UNCAUGHT EXCEPTION\n' + err)
 });
-let canBeCalled = true;
-let canBeCalled2 = true;
-let canBeCalled3 = true;
-let appID = ``;
-let appSecret = ``;
-let postId = ``;
-let accessToken = ``;
-
-var facebook = new fbsdk.Facebook({
-    appId: appID,
-    secret: appSecret
+const facebook = new fbsdk.Facebook({
+  appId: appID,
+  secret: appSecret,
 });
+const source = new EventSource(`https://streaming-graph.facebook.com/${postId}/live_comments?access_token=${accessToken}&comment_rate=one_hundred_per_second&fields=from{name,id},message`);
 const clients = new Map();
-videos = new Map();
-facebook.api(appID, function (data) {
-    console.log(data);
+const videos = new Map();
+facebook.api(appID, function(data) {
+  console.log(data);
 });
-
-/*https.get(`https://streaming-graph.facebook.com/${postId}/live_comments?access_token=${accessToken}&comment_rate=ten_per_second&fields=from{name,id},message`, res => {
-    console.log('statusCode:', res.statusCode);
-    res.on('data', (d) => {
-        data = data + d;
-    });
-    res.on('end', () => {
-
-            console.log(data)
-
-
-    })
-});*/
-
-/*setTimeout(function() {
-    wsHtml(`https://youtu.be/n0U5DrzD2Wo`);
-}, 2000);
-
-setTimeout(function() {
-    wsHtml(`https://www.youtube.com/watch?v=eEa3vDXatXg`);
-}, 4000);
-
-setTimeout(function() {
-    wsHtml(`https://www.youtube.com/watch?v=RLphoP6APkg`);
-}, 6000);
-*/
-
-var source = new EventSource(`https://streaming-graph.facebook.com/${postId}/live_comments?access_token=${accessToken}&comment_rate=one_hundred_per_second&fields=from{name,id},message`);
-source.onopen = function(event) {
-    console.log(event);
-    console.log('connected');
-    source.onerror = function(err){
-        console.log(err);
-    };
-    source.onmessage = function(event) {
-    let info = JSON.parse(event.data);
-    console.log('got a message');
-      check = info.message.toString();
-      httpCheck = check.slice(0, 4);
-      if (httpCheck == 'http') {
-          wsHtml(check);
-      } else if (httpCheck == 'www.') {
-		  wsHtml(check);
-	  } else if (httpCheck == 'Skip') {
-        wsSkip();
-      } else if (httpCheck == 'skip') {
-        wsSkip();
-      } else if (httpCheck == 'SKIP') {
-        wsSkip();
-	  }	 else {
-		console.log(`wsAlert: ${info.message}`);
-        wsAlert(info.message);
-      }
-    };
-}
-
-ws.on('connection', function connection(ws, req) {
-    const clientIP = req.connection.remoteAddress;
-    const iden = Math.floor(Math.random() * 999999);
-    clients.set(iden, {
-        socket: ws,
-        IP: clientIP
-    });
-
-    console.log(`${clientIP} just connected.`);
-    console.log(`readyState: ${ws.readyState}`);
-    ws.on('message', function incoming(message) {
-        console.log(`websocket: ${message}`);
-        console.log(`readyState: ${ws.readyState}`);
-        wsAlert(message);
-    });
-});
-
-
-wsBroadcast = (data) => {
-
-    clients.forEach(function (client) {
-        console.log(client.socket.readyState);
-        if(client.socket.readyState == 1) {
-            client.socket.send(data);
-        }
-    });
-};
-
-wsAlert = (alertStr) => {
-    /*if(canBeCalled) {
-        canBeCalled = false; */
-		
-        newAlert = JSON.stringify({
-            alert: alertStr
-        });
-        wsBroadcast(newAlert);
-		
-        /* setTimeout(function() {
-            canBeCalled = true;
-        }, 1000)
+source.onopen = (event) => {
+  console.log(event);
+  console.log('connected');
+  source.onerror = (err) => {
+    console.log(err);
+  };
+  source.onmessage = (event) => {
+    const info = JSON.parse(event.data);
+    const check = info.message.toString();
+    const httpCheck = check.slice(0, 4);
+    if (httpCheck == 'http') {
+      wsHtml(check);
+    } else if (httpCheck == 'www.') {
+      wsHtml(check);
+    } else if (httpCheck == 'Skip') {
+      wsSkip();
+    } else if (httpCheck == 'skip') {
+      wsSkip();
+    } else if (httpCheck == 'SKIP') {
+      wsSkip();
     } else {
-        console.log('wsAlert: Rate limited.');
-    } */
-
-}
-
-wsHtml = (link) => {
-	let data;
-    if(canBeCalled2) {
-        canBeCalled2 = false;
-        setTimeout(function() {
-            canBeCalled2 = true;
-        }, 1000);
-        html = link;
-        videoId = html.slice(html.length - 11, html.length);
-        html = `https://www.youtube.com/watch?v=${videoId}`;
-        console.log(html);
-        if(videos.has(videoId)) {
-            console.log(`Video has already been played.`);
-            return;
-        } else {
-            videos.set(videoId);
-        }
-        https.get(html, res => {
-            console.log('statusCode:', res.statusCode);
-            res.on('data', (d) => {
-                data = data + d;
-            });
-            res.on('end', () => {
-                    data = data.toString();
-                    subCheck = `Licensed to YouTube by`;
-                    console.log('end');
-                    if(data.includes(subCheck)) {
-                        console.log(`There is copyrighted music in this video.`);
-                        return;
-                    }
-                    console.log(videoId);
-                    newData = JSON.stringify({
-                        html: videoId
-                    });
-                    wsBroadcast(newData);
-    
-            })
-        });
-    } else {
-        console.log('wsHtml: Rate limited.');
+      wsAlert(info.message);
     }
-
-}
-
-wsSkip = () => {
-    if(canBeCalled3) {
-        canBeCalled3 = false;
-        setTimeout(function() {
-            canBeCalled3 = true;
-        }, 500); 
-        console.log(`sending skip`);
-        newData = JSON.stringify({
-            skip: 'skip'
+  };
+};
+ws.on('connection', function connection(ws, req) {
+  const clientIP = req.connection.remoteAddress;
+  const iden = Math.floor(Math.random() * 999999);
+  clients.set(iden, {
+    socket: ws,
+    IP: clientIP,
+  });
+  ws.on('message', function incoming(message) {
+    wsAlert(message);
+  });
+});
+const wsBroadcast = (data) => {
+  clients.forEach(function(client) {
+    if (client.socket.readyState == 1) {
+      client.socket.send(data);
+    }
+  });
+};
+const wsAlert = (alertStr) => {
+  const newAlert = JSON.stringify({
+    alert: alertStr,
+  });
+  wsBroadcast(newAlert);
+};
+const wsHtml = (link) => {
+  let data;
+    let html = link;
+    const videoId = html.slice(html.length - 11, html.length);
+    html = `https://www.youtube.com/watch?v=${videoId}`;
+    console.log(html);
+    if (videos.has(videoId)) {
+      console.log(`Video has already been played.`);
+      return;
+    } else {
+      videos.set(videoId);
+    }
+    https.get(html, (res) => {
+      console.log('statusCode:', res.statusCode);
+      res.on('data', (d) => {
+        data = data + d;
+      });
+      res.on('end', () => {
+        data = data.toString();
+        console.log('end');
+        if (data.includes(`Licensed to YouTube by`)) {
+          console.log(`There is copyrighted music in this video.`);
+          return;
+        }
+        if (data.includes('Unlisted')) {
+          console.log(`Unlisted`);
+          return;
+        }
+        if (data.includes('Age-restricted')) {
+          console.log(`Age-restricted`);
+          return;
+        }
+        const newData = JSON.stringify({
+          html: videoId,
         });
         wsBroadcast(newData);
-    } else {
-        console.log('wsSkip: Rate limited.');
-    }
-
-}
-
-
-/*setInterval(function() {
-    videos.clear();
-}, 600000);*/
+      });
+    });
+};
+const wsSkip = () => {
+    console.log(`SKIP`);
+    const newData = JSON.stringify({
+      skip: 'skip',
+    });
+    wsBroadcast(newData);
+};
+setInterval(function() {
+  videos.clear();
+}, 1200000);
 app.use(cors());
-app.use("/", express.static(__dirname + '/public/'));
+app.use('/', express.static(__dirname + '/public/'));
 webServer.listen(7004, function listening() {
-    console.log('Listening on %d', webServer.address().port);
+  console.log('Listening on %d', webServer.address().port);
 });
